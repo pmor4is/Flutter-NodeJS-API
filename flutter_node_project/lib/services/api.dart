21e66126855813035c10931dfda0ca700f35cd01
Pedro Morais <pmor4is@gmail.com>
2023-11-09 01:04:25 -0300

    working on API

--- flutter_node_project/lib/services/api.dart
+++ flutter_node_project/lib/services/api.dart
@@ -1,3 +1,19 @@
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
 class Api {
-  
+  static const baseUrl = "";
+
+  static addProduct (Map productData) async {
+    try {
+      final res = await http.post(Uri.parse(uri), productData);
+      if (res.statusCode == 200) {
+        //
+      } else {
+        //
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
 }
