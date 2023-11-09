e4a911ca6d29aa41138be2675b06f6b01e97ca10
Pedro Morais <pmor4is@gmail.com>
2023-11-09 01:13:57 -0300

    working on API, post method

--- node_server/index.js
+++ node_server/index.js
@@ -2,6 +2,31 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+const productData = [];
+
+
+//post API
+app.post("/api/add_product",(req,res => {
+    console.log("Result", req.body);
+
+    const productData = {
+        "id": productData.lenght+1,
+        "productName": req.body.productName,
+        "productPrice": req.body.productPrice,
+        "productDescription": req.body.productDescription,
+    };
+
+    productData.push(productData);
+    console.log("final", productData);
+}))
+
+
 app.listen(2000, () => {
     console.log("listening on port 2000");
 })
